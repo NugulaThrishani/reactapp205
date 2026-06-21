25d8ba54bf810e270f09289b3ace7ad91b3957c4
NugulaThrishani <thrishanichowdary@gmail.com>
2026-06-21 17:44:01 +0530

    Updated homepage content

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ function App() {
     <>
       <h1 align="center">Hii Welcome to React App</h1>
       <h2 align="center">Level up!!</h2>
-      <h3 align="center">I am Thrishani Chowdary!</h3>
+      <h3 align="center">GIT ACTIONS are Done!!</h3>
+      <h3 align="center">This is simple deployed home page!!</h3>
+
     </>
   );
 }
