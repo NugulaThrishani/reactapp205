3d841e452393f98f64345dd95a00bae3f7e66a1d
NugulaThrishani <thrishanichowdary@gmail.com>
2026-06-21 17:14:34 +0530

    added new lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <h1 align="center">Hii Welcome to React App</h1>
+    <h1 align="center">Hii Welcome to React App</h1>,
+    <h2 align="center"> Level up!!</h2>,
+    <h3 align="center">I am Thrishani Chowdary!</h3>
   );
 }
 
